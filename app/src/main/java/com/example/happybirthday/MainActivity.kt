fadf3d6f75d6747ca0f794fc13ccb702f587cf74
UnkeptBedroom <wilfredo.martinez52@outlook.com>
2025-12-13 10:50:06 -0600

    From Mac

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    
                 }
             }
         }
